8549b5dbd1993349bb83fe25b036bec7918aa915
Sean Tycho <stycho@gmail.com>
2018-04-30 18:58:01 -0700

    commit 2

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 test('should render header correctly', () => {
   const wrapper = shallow(<Header />);
-  expect(toJSON(wrapper)).toMatchSnapshot();u
+  expect(wrapper).toMatchSnapshot();
   // const renderer = new ReactShallowRenderer();
   // renderer.render(<Header />);
   // expect(renderer.getRenderOutput()).toMatchSnapshot()
-}); 
+});
